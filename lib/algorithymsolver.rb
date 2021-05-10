0ef73ca4428f3cbbfaa258eb572e445f3b76a05e
paulo-felipe <alparakleto@gmail.com>
2021-05-10 09:09:00 -0300

    chore(algorithymsolver.rb): follow robocop instructions

--- lib/algorithymsolver.rb
+++ lib/algorithymsolver.rb
@@ -34,11 +34,11 @@ module Algorithymsolver
     def maximize
       @score = 0
       sorted_array = @array.sort
-      @negative_array = sorted_array.select { |number| number < 0 }
+      @negative_array = sorted_array.select { |number| number.negative? }
       @positive_array = sorted_array.select { |number| number > 0 }
 
       @negative_array.map! do |number|
-        number = number * (-1)
+        number = number * -1
         @score += 1
         if @score == @k
           return @negative_array.sum + @positive_array.sum
