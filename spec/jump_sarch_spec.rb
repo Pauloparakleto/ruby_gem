c3ffb1e3a17bb717ec674968775c528c3c6ab345
paulo-felipe <alparakleto@gmail.com>
2021-05-12 08:22:30 -0300

    fix(jump_saerch_spec.rb): rename Class

--- spec/jump_sarch_spec.rb
+++ spec/jump_sarch_spec.rb
@@ -4,18 +4,18 @@ RSpec.describe Algorithymsolver::JumpSearch do
   it "should sum 10" do
     array = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610]
     number = 55
-    expect(Algorithymsolver::MaximizeArray.new(array, number).jump_search).to eql(10)
+    expect(Algorithymsolver::JumpSearch.new(array, number).search).to eql(10)
   end
 
   it "should sum 20" do
     array = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610]
     number = 610
-    expect(Algorithymsolver::MaximizeArray.new(array, number).jump_search).to eql(15)
+    expect(Algorithymsolver::JumpSearch.new(array, number).search).to eql(15)
   end
 
   it "should sum 20" do
     array = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610]
     number = 200
-    expect(Algorithymsolver::MaximizeArray.new(array, number).jump_search).to eql(-1)
+    expect(Algorithymsolver::JumpSearch.new(array, number).search).to eql(-1)
   end
 end
